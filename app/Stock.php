9d35ddbd9d645297eed3a61521360a335f3898f0
Jayant Gandhi <jayant.gandhi127@gmail.com>
2017-07-21 11:50:35 -0400

    search methods on stock

--- app/Stock.php
+++ app/Stock.php
@@ -11,6 +11,20 @@ class Stock extends Model
       return 'slug';
     }
 
+    public function scopeSearchBySymbol($query, $symbol)
+    {
+      if (!empty($symbol)) {
+        return $query->where('symbol', 'LIKE', "%{$symbol}%");
+      }
+    }
+
+    public function scopeSearchByName($query, $name)
+    {
+      if (!empty($name)) {
+        return $query->where('name', 'LIKE', "%{$name}%");
+      }
+    }
+
     public function createSlug()
     {
       $this->slug = str_slug($this->symbol);
